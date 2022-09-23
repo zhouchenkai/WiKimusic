c69defc6b97a707abd42b65edf34b68c5f0c1711
zhouck <240046220@qq.com>
2022-09-23 09:49:53 +0800

    first change

---
+++ WiKiMUsic/WiKiMUsic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WiKiMUsic
+//
+//  Created by 周晨凯 on 2022/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
